60ea9877c57c92ba378516e74d486a9120b76580
hobospider132 <christian.duong44@gmail.com>
2023-11-27 16:56:17 +1100

    Need to finish up DST detection

--- JS/clock.js
+++ JS/clock.js
@@ -1,13 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+// code for clock
+
+document.addEventListener('DOMContentLoaded', function () {
+    
     async function delay(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     async function show() {
         await delay(890);
-        timeDiv.style.display = 'block';
         timeFill.style.display = 'none';
-    } 
+        timeDiv.style.display = 'block';
+    }
 
     let hour = document.getElementById('hour');
     let min = document.getElementById('min');
@@ -22,14 +25,20 @@ document.addEventListener('DOMContentLoaded', function() {
     let timeFill = document.getElementById('time-filler');
 
     timeDiv.style.display = 'none';
-    timeFill.style.display = 'block';
+
+    function isDST() {
+        const options = { timeZone: 'Australia/Sydney' };
+        const today = new Date().toLocaleString('en-GB', options);
+        const startDST = new Date(undefined, 9, 1); // October 1st
+        const endDST = new Date(undefined, 3, 7);   // April 7th
+        
+        return today >= startDST.toLocaleString('en-GB', options) &&
+               today <= endDST.toLocaleString('en-GB', options);
+    }
 
     function dateNow() {
-        const currentDate = new Date();
-        const isDST = isDaylightSavingTime(currentDate);
-
         const options = { timeZone: 'Australia/Sydney', hour12: false };
-        const rn = currentDate.toLocaleString('en-GB', options);
+        const rn = new Date().toLocaleString('en-GB', options);
 
         const parts = rn.split(', ');
         const [dateStr, timeStr] = parts;
@@ -37,47 +46,49 @@ document.addEventListener('DOMContentLoaded', function() {
         const [monthNow, dayNow, yearNow] = dateStr.split('/');
         let [hrNow, minNow, secNow] = timeStr.split(':');
 
+        const isDaylightSavingTime = isDST();
+        DSTZone = isDaylightSavingTime ? "AEDT" : "AEST";
+        UTCDiff = isDaylightSavingTime ? 11 : 10;
+
         month.innerHTML = monthNow;
         day.innerHTML = dayNow;
-        year.innerHTML = `${yearNow} | ${isDST ? 'AEDT' : 'AEST'} time`;
+        year.innerHTML = `${yearNow} |  ${DSTZone}`;
+        console.log(DSTZone);
 
         if (hrNow > 12) {
             hrNow = hrNow - 12;
         }
-
+        
         if (hrNow == 0) {
             hrNow = 12;
         }
-
+        
         hour.innerHTML = hrNow;
         sec.innerHTML = String(secNow).padStart(2, '0');
         min.innerHTML = String(minNow).padStart(2, '0');
 
         // time for the user (used to calculate offset for user)
         const userOptions = { hour12: false };
-        const userRn = currentDate.toLocaleString('en-GB', userOptions);
+        const userRn = new Date().toLocaleString('en-GB', userOptions);
 
-        const diffMins = (currentDate - new Date(rn)) / (1000 * 60); // convert to mins
+        const diffMins = (new Date(userRn) - new Date(rn)) / (1000 * 60); // convert to mins
         const diffHours = diffMins / 60; // convert to hours
 
-        let diff = document.getElementById('diff');
+        let diff;
 
         if (diffHours < 0) {
-            diff = `${Math.abs(diffHours)} hours behind | UTC +${isDST ? 11 : 10}`;
-        } else if (diffHours > 0) {
-            diff = `${diffHours} hours ahead | UTC +${isDST ? 11 : 10}`;
-        // } else {
-        //     diff = ""; 
-        // } 
+           diff = `${Math.abs(diffHours)} hours behind | UTC +${UTCDiff}`;
+           console.log(UTCDiff);
+        }else if(diffHours > 0){
+           diff = `${diffHours} hours ahead | UTC +{UTCDiff}`;
+           console.log(UTCDiff);
+        } else{
+           diff = `UTC +${UTCDiff}`;
+           console.log(UTCDiff);
+        }
 
-        diff.innerHTML = diff;
-    }
+        timeDiff.innerHTML = diff;
 
-    // Function to check if daylight saving time is currently observed
-    function isDaylightSavingTime(date) {
-        const january = new Date(date.getFullYear(), 0, 1);
-        const july = new Date(date.getFullYear(), 6, 1);
-        return date.getTimezoneOffset() < Math.max(january.getTimezoneOffset(), july.getTimezoneOffset());
     }
 
     setInterval(dateNow, 1000);
