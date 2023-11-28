7e8c49d49dd208a76e69ec62a8e3a18f76158938
hobospider132 <christian.duong44@gmail.com>
2023-11-28 11:10:15 +1100

    Added DST detection, can finally sleep now

--- JS/clock.js
+++ JS/clock.js
@@ -1,6 +1,10 @@
 // code for clock
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    var timeDiv = document.getElementById('time');
+    var timeFill = document.getElementById('time-filler');
+    timeDiv.style.display = 'none';
     
     async function delay(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -8,41 +12,48 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function show() {
         await delay(890);
-        timeFill.style.display = 'none';
+        timeFill.style.display = 'none'
         timeDiv.style.display = 'block';
     }
 
-    let hour = document.getElementById('hour');
-    let min = document.getElementById('min');
-    let sec = document.getElementById('sec');
-
-    let month = document.getElementById('month');
-    let day = document.getElementById('day');
-    let year = document.getElementById('year');
-    let timeDiff = document.getElementById('diff');
-
-    let timeDiv = document.getElementById('time');
-    let timeFill = document.getElementById('time-filler');
-
-    timeDiv.style.display = 'none';
-
     function isDST() {
-        const options = { timeZone: 'Australia/Sydney' };
-        const today = new Date().toLocaleString('en-GB', options);
-        const startDST = new Date(undefined, 9, 1); // October 1st
-        const endDST = new Date(undefined, 3, 7);   // April 7th
-        
-        return today >= startDST.toLocaleString('en-GB', options) &&
-               today <= endDST.toLocaleString('en-GB', options);
+        const AusOptions = { timeZone: 'Australia/Sydney', hour12: true };
+        const AusToday = new Date().toLocaleString('en-GB', AusOptions);
+
+        const [, AusTime]  = AusToday.split(', ');
+
+        let [AusHr] = AusTime.split(':');
+
+        const UTCOptions = { timeZone: 'UTC', hour12: true};
+        const UTCToday = new Date().toLocaleString('en-GB', UTCOptions)
+
+        const [, UTCTime] = UTCToday.split(', ');
+
+        let [UTCHr] = UTCTime.split(":");
+
+        let num_AusHr = Number(AusHr);
+        let num_UTCHr = Number(UTCHr);
+
+        return num_AusHr - num_UTCHr === -1;
     }
 
     function dateNow() {
+
+        let hour = document.getElementById('hour');
+        let min = document.getElementById('min');
+        let sec = document.getElementById('sec');
+    
+        let month = document.getElementById('month');
+        let day = document.getElementById('day');
+        let year = document.getElementById('year');
+        let timeDiff = document.getElementById('diff');
+    
         const options = { timeZone: 'Australia/Sydney', hour12: false };
         const rn = new Date().toLocaleString('en-GB', options);
-
+    
         const parts = rn.split(', ');
         const [dateStr, timeStr] = parts;
-
+    
         const [monthNow, dayNow, yearNow] = dateStr.split('/');
         let [hrNow, minNow, secNow] = timeStr.split(':');
 
@@ -53,7 +64,6 @@ document.addEventListener('DOMContentLoaded', function () {
         month.innerHTML = monthNow;
         day.innerHTML = dayNow;
         year.innerHTML = `${yearNow} |  ${DSTZone}`;
-        console.log(DSTZone);
 
         if (hrNow > 12) {
             hrNow = hrNow - 12;
@@ -78,13 +88,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (diffHours < 0) {
            diff = `${Math.abs(diffHours)} hours behind | UTC +${UTCDiff}`;
-           console.log(UTCDiff);
         }else if(diffHours > 0){
            diff = `${diffHours} hours ahead | UTC +{UTCDiff}`;
-           console.log(UTCDiff);
         } else{
            diff = `UTC +${UTCDiff}`;
-           console.log(UTCDiff);
         }
 
         timeDiff.innerHTML = diff;
