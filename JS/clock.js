d72a20e9192a73a7077442e8aff1616411d5abad
Hoobs <Hobospider132@gmail.com>
2025-03-16 17:22:49 +1100

    Update clock.js

--- JS/clock.js
+++ JS/clock.js
@@ -106,3 +106,48 @@ document.addEventListener('DOMContentLoaded', function () {
     setInterval(dateNow, 1000);
     show();
 });
+
+
+// Not sure why index.js is not loading properly after switching to custom domain. Clock.js runs fine so the two will be merged until a fix is found
+/* Edit the page if it's on mobile so it works and everything fits (intended for iphone xr)*/
+
+function device() {
+    const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    return regex.test(navigator.userAgent);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener("load", event => {
+        const textElement = document.getElementById('visitorCount');
+        textElement.style.display = 'none';
+    });  
+
+    lozad('.lozad', {
+        load: function(el) {
+            el.src = el.dataset.src;
+        }
+    }).observe()
+
+    let contact = document.getElementById('contact-wrapper');
+    let main = document.getElementById('main');
+    let center = document.getElementById('center');
+    let timeDiv = document.getElementById('time');
+    let timeFill = document.getElementById('time-filler');	
+    let osuLink = document.getElementById('osu');
+    if (device()) { 
+        /* If it's on mobile */
+        contact.style.width = 'clamp(380px, 8vw, 105px)';
+        contact.style.marginLeft = '20px';
+        main.style.marginLeft = '30px';
+        center.style.alignContent = 'center';
+        timeDiv.style.marginLeft = '60px';
+        timeFill.style.marginLeft = '60px';
+        osuLink.src = "https://osu-sig.vercel.app/card?user=Hobospider132&mode=std&lang=en&round_avatar=true&animation=true&hue=125&mini=true"; 
+    } else {
+        /* If it's on desktop */
+        timeDiv.style.marginLeft = '340px';
+        timeFill.style.marginLeft = '340px';
+        osuLink.src = 'https://osu-sig.vercel.app/card?user=Hobospider132&mode=std&lang=en&round_avatar=true&animation=true&hue=200&skills=true';
+        
+    }   
+});
