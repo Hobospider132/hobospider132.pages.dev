dd73ba053033d0b085ffde2feb7ac8df4d5e0a94
Hoobs <Hobospider132@gmail.com>
2025-03-18 16:46:04 +1100

    Update displayAni.js

--- JS/displayAni.js
+++ JS/displayAni.js
@@ -1,3 +1,36 @@
+function displayAnilistData(data) {
+  const activityDiv = document.getElementById('ani-activity');
+  activityDiv.innerHTML = ''; 
+  activityDiv.style.backgroundImage = `url(${data.User.bannerImage})`;
+  activityDiv.style.backgroundSize = "contain";
+
+  data.Page.activities.forEach(activity => {
+    const activityCard = document.createElement('div');
+    activityCard.style = "margin: 5px; width: 50%; text-align: center; justify-self: center;";
+    activityCard.innerHTML = `
+      <img src="${activity.media.coverImage.medium}" alt="Cover Image" style="width: 50%; height: 50%;">
+      <h5>${activity.media.title.english || activity.media.title.romaji}</h5>
+      <p>Progress: ${activity.progress || "N/A"}</p>
+      <p>Status: ${activity.status || "Unknown"}</p>
+    `;
+
+    const link = document.createElement("a");
+    link.href = activity.media.siteUrl;
+    link.target = "_blank";
+    link.appendChild(activityCard);
+    activityDiv.appendChild(link);
+  });
+
+  const user = data.User;
+  const accountDiv = document.getElementById('anilist-account');
+  accountDiv.style.justifySelf = "center";
+  accountDiv.innerHTML = `
+    <img src="${user.avatar.medium}" alt="Avatar" class="profile-img-ani">
+    <h4 style="padding: 0;">${user.name}</h4>
+    <p>${user.about || "No bio available"}</p>
+  `;
+}
+
 const query = `
   query {
     Page(page: 1, perPage: 4) {
@@ -5,14 +38,13 @@ const query = `
         ... on ListActivity {
           progress
           status
-          createdAt
           media {
             coverImage {
               medium
             }
             title {
-                english
-                romaji
+              english
+              romaji
             }
             siteUrl
           }
@@ -35,7 +67,7 @@ const destination = {
   headers: {
     "Content-Type": "application/json"
   },
-  body: JSON.stringify({ query }) 
+  body: JSON.stringify({ query })
 };
 
 fetch('https://graphql.anilist.co', destination)
@@ -45,34 +77,3 @@ fetch('https://graphql.anilist.co', destination)
     displayAnilistData(data.data);
   })
   .catch(error => console.error('GraphQL error:', error));
-
-function displayAnilistData(data) {
-  const user = data.User;
-  const accountDiv = document.getElementById('anilist-account');
-  accountDiv.style = "justify-self: center";
-  accountDiv.innerHTML = `
-    <img src="${user.avatar.medium}" alt="Avatar" class="profile-img-ani">
-    <h4 style="padding: 0px">${user.name}</h4>
-    <p>${user.about || "No bio available"}</p>
-  `;
-
-  const activityDiv = document.getElementById('ani-activity');
-  activityDiv.innerHTML = ''; 
-  activityDiv.style.backgroundImage=`url(${user.bannerImage})`;
-  activityDiv.style.backgroundImage="contain";
-  data.Page.activities.forEach(activity => {
-    const activityCard = document.createElement('div');
-    activityCard.style = "margin: 5px; width: 50px; text-align: center; justify-self: center";
-    activityCard.innerHTML = `
-      <img src="${activity.media.coverImage.medium}" alt="Cover Image" style="width: 50%; height: 50%">
-      <h5>${activity.media.title.english || activity.media.title.romaji}</h5>
-      <p>Progress: ${activity.progress || "N/A"}</p>
-      <p>Status: ${activity.status || "Unknown"}</p>
-    `;
-    const link = document.createElement("a");
-    link.href = activity.mediasiteUrl;
-    link.target = "_blank";
-    activityDiv.appendChild(activityCard);
-    link.appendChild(activityDiv);
-  });
-}
