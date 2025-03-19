798010d88fcea99f5a6015884bfc63ddaad9d9cf
CDuong08 <christian.duong2@education.nsw.gov.au>
2025-03-19 13:51:48 +1100

    Increasing mobile friendlinesss

--- JS/index.js
+++ JS/index.js
@@ -14,75 +14,15 @@ document.addEventListener('DOMContentLoaded', function () {
     let contact = document.getElementById('contact-wrapper');
     let main = document.getElementById('main');
     let center = document.getElementById('center');
-    let timeDiv = document.getElementById('time');
-    let timeFill = document.getElementById('time-filler');	
     let osuLink = document.getElementById('osu');
     if (device()) { 
         /* If it's on mobile */
         contact.style.width = 'clamp(380px, 8vw, 105px)';
-        contact.style.marginLeft = '20px';
         main.style.marginLeft = '30px';
         center.style.alignContent = 'center';
-        timeDiv.style.marginLeft = '60px';
-        timeFill.style.marginLeft = '60px';
         osuLink.src = "https://osu-sig.vercel.app/card?user=Hobospider132&mode=std&lang=en&round_avatar=true&animation=true&hue=125&mini=true"; 
     } else {
         /* If it's on desktop */
-        timeDiv.style.marginLeft = '340px';
-        timeFill.style.marginLeft = '340px';
         osuLink.src = 'https://osu-sig.vercel.app/card?user=Hobospider132&mode=std&lang=en&round_avatar=true&animation=true&hue=200&skills=true';        
     }   
 });
-
-// From here it's gonna be code for anilist, busy irl but got this set up for now. Will do the rest at a later date
-//    const payload =  query stats {
-//       Page(page: 1, perPage: 5) {
-//         activities(userId: 6252093, sort: ID_DESC) {
-//           ... on ListActivity {
-//             id
-//             type
-//             status
-//             siteUrl
-//             createdAt
-//             user {
-//               updatedAt
-//             }
-//             media {
-//               bannerImage
-//               meanScore
-//               siteUrl
-//             }
-//           }
-//         }
-//       }
-//       User(id: 6252093) {
-//         bannerImage
-//         avatar {
-//           medium
-//         }
-//         about
-//         favourites {
-//           manga(page: 1, perPage: 3) {
-//             nodes {
-//               title {
-//                 romaji
-//               }
-//               bannerImage
-//               meanScore
-//               siteUrl
-//             }
-//           }
-//           anime(page: 1, perPage: 3) {
-//             nodes {
-//               title {
-//                 romaji
-//               }
-//               bannerImage
-//               meanScore
-//               siteUrl
-//             }
-//           }
-//         }
-//       }
-//     }
-// }
